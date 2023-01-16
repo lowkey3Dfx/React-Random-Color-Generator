053c7d33791132ed6a118d79e511a84d120e7b10
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-01-16 21:20:34 +0100

    Add random hex dependencie works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import * as randomHex from 'random-hex';
 import { useState } from 'react';
 
 function App() {
-  const [changeColor, setChangeColor] = useState()
+  const [changeColor, setChangeColor] = useState();
 
-  export default function changeColor(){
-    setChangeColor(() => )
-  }
+  // export default function changeColor(){
+  //   setChangeColor(() => )
+  // }
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: randomHex.generate() }}>
       <h1>This is a react random color generator</h1>
       <button onClick={changeColor}>Generate</button>
     </div>
